2fd163e6d5438afd46c4bb627833d0c7c6bb5bfd
ChoYunMin <dbsals0618@naver.com>
2016-01-25 16:19:50 +0900

    Update Stack.h

--- 2016 Winter Seminar/Data Structure/Stack & Queue/Stack.h
+++ 2016 Winter Seminar/Data Structure/Stack & Queue/Stack.h
@@ -35,4 +35,36 @@ public:
 	{
 		return vStack[nStackSize - 1];
 	}
+	
+	bool fnSearch(int iData) // iData가 stack에 있는지 탐색
+	{
+		int i;
+		for (i = 0; i < nStackSize; i++)
+		{
+			if (vStack[i] == iData)
+			{
+				return true;
+			}
+		}
+
+		if (i == nStackSize) // 일치하는 데이터가 없으면 false 반환
+		{
+			return false;
+		}
+	}
+
+	void fnAllDelete() // Stack 전체 삭제
+	{
+		for (int i = 0; i < nStackSize; i++)
+		{
+			vStack.pop_back();
+		}
+
+		return;
+	}
+
+	T fnStackSize() // Stack 개수 반환
+	{
+		return nStackSize;
+	}
 };
